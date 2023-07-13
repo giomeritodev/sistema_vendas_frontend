cbb5427983e91e2b1bd02b39d3e9541e3244292d
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2023-07-12 21:10:52 -0300

    insert hooks in insert product

---
+++ src/module/product/hooks/useInsertProduct.ts
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { InsertProduct } from '../../../shared/components/dtos/insertProduct.dto';
+import { URL_PRODUCT } from '../../../shared/constants/urls';
+import { connectionAPIPost } from '../../../shared/functions/connection/connectionAPI';
+import { useGlobalContext } from '../../../shared/hooks/useGlobalContext';
+import { ProductRouterEnum } from '../routes';
+
+export const useInsertProduct = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [disabledButton, setDisabledButton] = useState(true);
+  const { setNotification } = useGlobalContext();
+  const [product, setProduct] = useState<InsertProduct>({
+    name: '',
+    price: 0,
+    image: '',
+    weight: 0,
+    length: 0,
+    height: 0,
+    width: 0,
+    diameter: 0,
+  });
+
+  useEffect(() => {
+    if (product.name && product.categoryId && product.image && product.price > 0) {
+      setDisabledButton(false);
+    } else {
+      setDisabledButton(true);
+    }
+  }, [product]);
+
+  const onChangeInput = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    nameObject: string,
+    isNumber?: boolean,
+  ) => {
+    setProduct({
+      ...product,
+      [nameObject]: isNumber ? Number(event.target.value) : event.target.value,
+    });
+  };
+
+  const handleChangeSelect = (value: string) => {
+    setProduct({
+      ...product,
+      categoryId: Number(value),
+    });
+  };
+
+  const handleInsertProduct = async () => {
+    setLoading(true);
+    await connectionAPIPost(URL_PRODUCT, product)
+      .then(() => {
+        setNotification('Sucesso!', 'success', 'Produto inserido com sucesso.');
+        navigate(ProductRouterEnum.PRODUCT);
+      })
+      .catch((error: Error) => {
+        setNotification(error.message, 'error');
+      });
+    setLoading(false);
+  };
+
+  return {
+    product,
+    loading,
+    disabledButton,
+    onChangeInput,
+    handleInsertProduct,
+    handleChangeSelect,
+  };
+};
--- src/module/product/screens/ProductInsert.tsx
+++ src/module/product/screens/ProductInsert.tsx
@@ -1,36 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '../../../shared/components/buttons/button/button';
-import { InsertProduct } from '../../../shared/components/dtos/insertProduct.dto';
 import Input from '../../../shared/components/inputs/input/Input';
 import InputMoney from '../../../shared/components/inputs/input/inputMoney/inputMoney';
 import Select from '../../../shared/components/inputs/select/select';
 import Screen from '../../../shared/components/screen/Screen';
 import { DisplayFlexJustifyRight } from '../../../shared/components/styles/display.style';
 import { LimitedContainer } from '../../../shared/components/styles/limited.style';
-import { URL_CATEGORY, URL_PRODUCT } from '../../../shared/constants/urls';
+import { URL_CATEGORY } from '../../../shared/constants/urls';
 import { MethodsEnum } from '../../../shared/enums/methods.enum';
-import { connectionAPIPost } from '../../../shared/functions/connection/connectionAPI';
 import { useDataContext } from '../../../shared/hooks/useDataContext';
-import { useGlobalContext } from '../../../shared/hooks/useGlobalContext';
 import { UseRequests } from '../../../shared/hooks/useRequests';
+import { useInsertProduct } from '../hooks/useInsertProduct';
 import { ProductRouterEnum } from '../routes';
 import { ProductInsertContainer } from '../styles/productInsert.style';
 
 const ProductInsert = () => {
-  const [product, setProduct] = useState<InsertProduct>({
-    name: '',
-    price: 0,
-    image: '',
-    weight: 0,
-    length: 0,
-    height: 0,
-    width: 0,
-    diameter: 0,
-  });
+  const {
+    product,
+    loading,
+    disabledButton,
+    onChangeInput,
+    handleInsertProduct,
+    handleChangeSelect,
+  } = useInsertProduct();
+
   const { categories, setCategories } = useDataContext();
-  const { setNotification } = useGlobalContext();
+
   const { request } = UseRequests();
   const navigate = useNavigate();
 
@@ -40,37 +37,8 @@ const ProductInsert = () => {
     }
   }, []);
 
-  const handleInsertProduct = async () => {
-    await connectionAPIPost(URL_PRODUCT, product)
-      .then(() => {
-        setNotification('Sucesso!', 'success', 'Produto inserido com sucesso.');
-        navigate(ProductRouterEnum.PRODUCT);
-      })
-      .catch((error: Error) => {
-        setNotification(error.message, 'error');
-      });
-  };
-
   const handleOnClickCancel = () => {
     navigate(ProductRouterEnum.PRODUCT);
-  };
-
-  const onChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    nameObject: string,
-    isNumber?: boolean,
-  ) => {
-    setProduct({
-      ...product,
-      [nameObject]: isNumber ? Number(event.target.value) : event.target.value,
-    });
-  };
-
-  const handleChange = (value: string) => {
-    setProduct({
-      ...product,
-      categoryId: Number(value),
-    });
   };
 
   return (
@@ -91,21 +59,21 @@ const ProductInsert = () => {
       <ProductInsertContainer>
         <LimitedContainer width={400}>
           <Input
-            onChange={(event) => onChange(event, 'name')}
+            onChange={(event) => onChangeInput(event, 'name')}
             value={product.name}
             margin="0px 0px 16px 0px"
             title="Nome"
             placeholder="Nome"
           />
           <Input
-            onChange={(event) => onChange(event, 'image')}
+            onChange={(event) => onChangeInput(event, 'image')}
             value={product.image}
             margin="0px 0px 16px 0px"
             title="Url Image"
             placeholder="Url da Image"
           />
           <InputMoney
-            onChange={(event) => onChange(event, 'price', true)}
+            onChange={(event) => onChangeInput(event, 'price', true)}
             value={product.price}
             margin="0px 0px 16px 0px"
             title="Preço"
@@ -114,42 +82,42 @@ const ProductInsert = () => {
           <Select
             title="Categoria"
             margin="0px 0px 32px 0px"
-            onChange={handleChange}
+            onChange={handleChangeSelect}
             options={categories.map((category) => ({
               value: `${category.id}`,
               label: `${category.name}`,
             }))}
           />
           <Input
-            onChange={(event) => onChange(event, 'weight', true)}
+            onChange={(event) => onChangeInput(event, 'weight', true)}
             value={product.weight}
             margin="0px 0px 16px 0px"
             title="Peso"
             placeholder="Peso do item"
           />
           <Input
-            onChange={(event) => onChange(event, 'length', true)}
+            onChange={(event) => onChangeInput(event, 'length', true)}
             value={product.length}
             margin="0px 0px 16px 0px"
             title="Comprimento"
             placeholder="Comprimento do item"
           />
           <Input
-            onChange={(event) => onChange(event, 'height', true)}
+            onChange={(event) => onChangeInput(event, 'height', true)}
             value={product.height}
             margin="0px 0px 16px 0px"
             title="Altura"
             placeholder="Altura do item"
           />
           <Input
-            onChange={(event) => onChange(event, 'width', true)}
+            onChange={(event) => onChangeInput(event, 'width', true)}
             value={product.width}
             margin="0px 0px 16px 0px"
             title="Largura"
             placeholder="Largura do item"
           />
           <Input
-            onChange={(event) => onChange(event, 'diameter', true)}
+            onChange={(event) => onChangeInput(event, 'diameter', true)}
             value={product.diameter}
             margin="0px 0px 16px 0px"
             title="Diâmetro"
@@ -162,7 +130,12 @@ const ProductInsert = () => {
               </Button>
             </LimitedContainer>
             <LimitedContainer width={120}>
-              <Button onClick={handleInsertProduct} type="primary">
+              <Button
+                loading={loading}
+                disabled={disabledButton}
+                onClick={handleInsertProduct}
+                type="primary"
+              >
                 Inserir produto
               </Button>
             </LimitedContainer>
