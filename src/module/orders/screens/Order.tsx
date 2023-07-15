29477b592042b8f6028c59ebc7d00ba629d5b511
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2023-07-14 21:24:02 -0300

    insert table orders

--- src/module/orders/screens/Order.tsx
+++ src/module/orders/screens/Order.tsx
@@ -1,14 +1,53 @@
+import { ColumnsType } from 'antd/es/table';
+
 import Screen from '../../../shared/components/screen/Screen';
+import Table from '../../../shared/components/table/Table';
+import { OrderType } from '../../../shared/types/OrderType';
 import { useOrder } from '../hooks/useOrder';
 
 const Order = () => {
   const { orders } = useOrder();
 
-  console.log('', orders);
+  const columns: ColumnsType<OrderType> = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text) => <p>{text}</p>,
+    },
+    {
+      title: 'Data',
+      dataIndex: 'date',
+      key: 'date',
+      sorter: (a, b) => a.date.localeCompare(b.date), //ordenar
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Usuário',
+      dataIndex: 'user',
+      key: 'user',
+      render: (__, target) => <p>{target.user?.name}</p>,
+    },
+    {
+      title: 'Qtd. Produtos',
+      dataIndex: 'amountProducts',
+      key: 'amountProducts',
+      render: (text) => <p>{text}</p>,
+    },
+  ];
 
   return (
-    <Screen>
-      <div>Ordens</div>
+    <Screen
+      listBreadcrumb={[
+        {
+          name: 'HOME',
+        },
+        {
+          name: 'PEDIDOS',
+        },
+      ]}
+    >
+      <Table columns={columns} dataSource={orders} />
     </Screen>
   );
 };
